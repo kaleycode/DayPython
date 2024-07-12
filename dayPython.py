12222a328daea5a842c08eae6c167e2e9c8239dc
kaleycode <readingkale@gmail.com>
2024-07-12 10:55:51 -0700

    Add files via upload

---
+++ dayPython.py
@@ -0,0 +1,19 @@
+userName = input("Hello there! What is you name?")
+print("Hello, " + userName + "!")
+dayOfWeek = input("Can you tell me what day of the week it is?")
+if dayOfWeek == "Monday" or dayOfWeek == "monday":
+  print("I hope this week is smooth for you!")
+elif dayOfWeek =="Tuesday" or dayOfWeek == "tuesday":
+  print("Oh! Are you having tacos? Because you should totally have tacos!")
+elif dayOfWeek =="Wednesday" or dayOfWeek == "wednesday":
+  print("Happy hump day!")
+elif dayOfWeek =="Thursday" or dayOfWeek == "thursday":
+  print("Aha! almost Friday! You got this!")
+elif dayOfWeek =="Friday" or dayOfWeek == "friday":
+  print("Happy Friday! You made it!")
+elif dayOfWeek =="Saturday" or dayOfWeek == "saturday":
+  print("I hope you are having an awesome weekend so far! You totally deserve it!")
+elif dayOfWeek =="Sunday" or dayOfWeek == "sunday":
+  print("Hopefully you're having a peaceful Sunday! Enjoy the weekened while it lasts!")
+else:
+  print("That's not a day of the week! Try again!")
